23993e694540d7fda5b1e31db38726fecef11947
Denys Desmecht <d.desmecht@stag.technofuturtic.education>
2023-06-05 17:03:34 +0200

    Ajoutez des fichiers projet.

---
+++ Exo1/Models/Film.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exo1.Models
+{
+    public enum GenreFilm
+    {
+        Action, Aventure, Romantique, Comédie, Thriller, Horreur, Indépendant, BioPic, Historique
+    }
+    public class Film
+    {
+        public int ID { get; set; } //autoincrémenté
+        public string Title { get; set; }//unique
+        public DateTime Director { get; set; }
+        public string MainActor { get; set; }
+        public GenreFilm GenreFilm { get; set; }
+
+    }
+}
